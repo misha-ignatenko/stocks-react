8d85447262bf844a6b0bfdc01aff9eaab66405a8
Misha Ignatenko <mykhaylo.ignatenko@gmail.com>
2015-11-19 21:57:31 -0600

    got rid of unused graph

--- client/components/StocksGraph.jsx
+++ client/components/StocksGraph.jsx
@@ -1,11 +1,5 @@
 StocksGraph = React.createClass({
 
-    getInitialState: function() {
-        return {
-            //chartInstance: null
-        }
-    },
-
     propTypes: {
         stocksToGraphObjects: React.PropTypes.array.isRequired
     },
@@ -16,57 +10,6 @@ StocksGraph = React.createClass({
 
     initializeChart: function(stocksObjectsArray) {
         console.log("inside initialize chart. stocks object to graph: ", stocksObjectsArray);
-        var chartModel = {
-            chart: {
-                renderTo: 'myChart',
-                type: 'line'
-            },
-            credits: {
-                enabled: false
-            },
-            title: {
-                text: 'Historic World Population by Region'
-            },
-            tooltip: {
-                formatter: function () {
-                    return this.x + '\n' + this.series.name + ': ' + this.y + ' $';
-                }
-            },
-            plotOptions: {
-                series: {
-                    enableMouseTracking: true,
-                    stickyTracking: false,
-                    tooltip: {}
-                }
-            },
-            xAxis: {
-                title: {
-                    text: 'dates'
-                },
-                //TODO FIX THIS -- dates are glitchy
-                type: 'datetime'
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Stock price, $'
-                },
-                labels: {
-                    overflow: 'justify'
-                }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'top',
-                x: -100,
-                y: 100,
-                floating: true,
-                borderWidth: 1,
-                backgroundColor: '#FFFFFF',
-                shadow: true
-            }
-        };
 
         var seriesModel = [];
         stocksObjectsArray.forEach(function (obj) {
@@ -84,26 +27,6 @@ StocksGraph = React.createClass({
                 });
             }
         });
-        var width = this.props.width || null;
-        var height = this.props.height || null;
-        var selector = this.refs.myChart.getDOMNode();
-
-        var chartOptions = React.addons.update(chartModel, {
-            chart: {
-                renderTo: {$set: selector},
-                width: {$set: width},
-                height: {$set: height}
-            },
-            series: {$set: seriesModel}
-        });
-
-        var chartInstance = new Highcharts.Chart(chartOptions);
-        this.setState({
-            chartInstance: chartInstance
-        });
-
-
-
 
         $(this.refs.myChartTwo.getDOMNode()).highcharts('StockChart', {
 
@@ -126,11 +49,6 @@ StocksGraph = React.createClass({
 
             series: seriesModel
         });
-
-
-
-
-
     },
 
     componentDidMount: function() {
@@ -140,7 +58,6 @@ StocksGraph = React.createClass({
     render: function() {
         return (
             <div>
-                <div ref="myChart"></div>
                 <div ref="myChartTwo"></div>
             </div>
         );
