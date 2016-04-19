57bc7458fa6148589fe7c51eedce5733e99c9b0b
Mykhaylo Ignatenko <mykhaylo.ignatenko@gmail.com>
2016-04-18 23:15:52 -0500

    got rid of collection observers -- the app would not have been lightweight

--- server/collectionObservers/collectionObservers.js
+++ server/collectionObservers/collectionObservers.js
@@ -1,10 +1,10 @@
-Meteor.startup(function() {
-    StockPrices.find().observe({
-        added: function(document) {
-            console.log("new stock prices object added: ", document.symbol);
-        },
-        removed: function(oldDocument) {
-            console.log("new stock prices object REMOVED: ", oldDocument.symbol);
-        }
-    })
-});
+//Meteor.startup(function() {
+//    StockPrices.find().observe({
+//        added: function(document) {
+//            console.log("new stock prices object added: ", document.symbol);
+//        },
+//        removed: function(oldDocument) {
+//            console.log("new stock prices object REMOVED: ", oldDocument.symbol);
+//        }
+//    })
+//});
