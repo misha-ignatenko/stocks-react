2ffbcb2ec97fb99ea163bb115ff5fe40dd8e0697
Mykhaylo Ignatenko <mykhaylo.ignatenko@gmail.com>
2017-06-14 19:34:44 -0700

    removed unnecessary code

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,4 @@
 App = React.createClass({
-    componentDidMount: function() {
-        //TODO: get rid of this when deployed.
-        //Meteor.loginWithPassword("mignatenko", "mignatenko");
-    },
     render() {
         return (
             <div>
