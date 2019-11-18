fd82be1db04301b4fbec136ee68cbe45d99aec81
Mykhaylo Ignatenko <mykhaylo.ignatenko@gmail.com>
2019-11-17 16:05:58 -0800

    removed unused code

--- server/scheduledDataPulls.js
+++ server/scheduledDataPulls.js
@@ -14,65 +14,6 @@ Meteor.startup(function() {
         var _setting = Settings.findOne();
         var _dataAutoPullIsOn = _setting && _setting.dataImports && _setting.dataImports.autoDataImportsTurnedOn;
 
-
-
-        // pull prices for the day from Quandl Finance
-        var _nycDateTime = moment().tz("America/New_York");
-        var _nycDateTimeString = _nycDateTime.format();
-        var _nycTimeString = _nycDateTimeString.substring(11, _nycDateTimeString.length - 1 - 5) + ".000";
-        var _dateStringNyc = _nycDateTime.format("YYYY-MM-DD");
-        var _weekdayNyc = _nycDateTime.isoWeekday();
-        // 6 is Saturday
-        // 7 is Sunday
-        if (_weekdayNyc === 6 || _weekdayNyc === 7) {
-            // do nothing
-        } else {
-            var _lastPullDate = _setting && _setting.dataImports && _setting.dataImports.quandlPrices.lastPullDate &&
-                _setting.dataImports.quandlPrices.lastPullDate;
-            var _dailyYFpullTime = _setting.dataImports.quandlPrices.dailyPullTimeNyc;
-
-            if (false && _dataAutoPullIsOn && _lastPullDate !== _dateStringNyc && _nycTimeString >= _dailyYFpullTime) {
-                Settings.update({_id: _setting._id}, {$set: {"dataImports.quandlPrices.lastPullDate": _dateStringNyc}});
-
-                Email.send({
-                    to: Settings.findOne().serverSettings.ratingsChanges.emailTo,
-                    from: Settings.findOne().serverSettings.ratingsChanges.emailTo,
-                    subject: "pulling prices for the day from Yahoo Finance",
-                    text: JSON.stringify({
-                        timeNow: new Date(),
-                        nycDate: _dateStringNyc,
-                        weekDayNum: _weekdayNyc,
-                        lastPullDate: _lastPullDate,
-                        nycTimeString: _nycTimeString,
-                        dailyPullTimeNyc: _dailyYFpullTime
-                    })
-                });
-
-                // first, get stock prices from the free Quandl (Wiki)
-                Meteor.call("getQuandlPricesForDate", _dateStringNyc, false, false);
-
-                // second, pull prices from the paid Quandl (NASDAQ)
-                Meteor.call("getLatestPricesForAllSymbols", "2014-01-01", _dateStringNyc);
-
-                Email.send({
-                    to: Settings.findOne().serverSettings.ratingsChanges.emailTo,
-                    from: Settings.findOne().serverSettings.ratingsChanges.emailTo,
-                    subject: "DONE pulling prices for the day from Yahoo Finance",
-                    text: JSON.stringify({
-                        timeNow: new Date(),
-                        nycDate: _dateStringNyc,
-                        weekDayNum: _weekdayNyc,
-                        lastPullDate: _lastPullDate,
-                        nycTimeString: _nycTimeString,
-                        dailyPullTimeNyc: _dailyYFpullTime
-                    })
-                });
-
-                Meteor.call("ensureAdjCloseExistsFor", _dateStringNyc);
-            }
-        }
-
-
         if (_dataAutoPullIsOn && _lastQuandlDatePull !== _dateString && _timeString >= _timeEveryDayInIsoToPull) {
             var _previousSettings = Settings.findOne();
             var _previousServerSettings = _previousSettings.serverSettings;
