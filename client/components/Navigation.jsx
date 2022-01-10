7498d8f0f572d7bdaeb1029e7a083105a34fdcda
Mykhaylo Ignatenko <mykhaylo.ignatenko@gmail.com>
2022-01-09 16:07:23 -0800

    fix routing

---
+++ client/components/Navigation.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { NavLink } from 'react-router-dom';
+
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
+
+class Navigation extends Component {
+
+    constructor(props) {
+        super(props);    
+    }
+
+    render() {
+        const showDataImportsTab = this.props?.showDataImportsTab;
+        const activeStyle = ({isActive}) => {
+            if (isActive) return {fontWeight: 'bold'};
+        };
+
+        return (
+            <div>
+                <header>
+                    <AccountsUIWrapper />
+                </header>
+
+                <NavLink to="/" style={activeStyle}>Portfolios</NavLink> |{' '}
+                <NavLink to="/stock" style={activeStyle}>Individual Stocks</NavLink> |{' '}
+                <NavLink to="/upcomingEarningsReleases" style={activeStyle}>Upcoming Earnings Releases</NavLink> |{' '}
+                {showDataImportsTab ? <NavLink to="/dataImports" style={activeStyle}>Data Imports</NavLink> : null}
+            </div>
+        );
+    }
+}
+
+export default withTracker((props) => {
+    const userID = Meteor.userId();
+    const user = Meteor.users.findOne(userID, {fields: {showDataImportsTab: 1}});
+    const showDataImportsTab = user?.showDataImportsTab;
+
+    return {showDataImportsTab};
+})(Navigation);
--- client/router.jsx
+++ client/router.jsx
@@ -1,30 +1,26 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Router, Route, Switch } from 'react-router';
-import history from 'history';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import StocksApp from '../client/components/StocksApp.jsx';
 import IndividualStock from '../client/components/IndividualStock.jsx';
-import UpDownGradesJSONDataImport from '../client/components/dataImports/UpDownGradesJSONDataImport.jsx';
-import EarningsReleasesJSONDataImport from '../client/components/dataImports/EarningsReleasesJSONDataImport.jsx';
-import ImportRatingScales from '../client/components/dataImports/ImportRatingScales.jsx';
-import Blob from '../client/components/Blob.jsx';
-
-const browserHistory = history.createBrowserHistory();
+import DataImportsMain from './components/dataImports/DataImportsMain.jsx';
+import UpcomingEarningsReleases from './components/UpcomingEarningsReleases.jsx';
+import Navigation from './components/Navigation.jsx';
 
 Meteor.startup(function() {
     let AppRoutes = (
         <div className="container">
-        <Router history={browserHistory}>
-            <Switch>
-                <Route exact path="/" component={StocksApp} />
-                <Route exact path="/stock" component={IndividualStock} />
-                <Route exact path="/dataimport/updowngrades" component={UpDownGradesJSONDataImport} />
-                <Route exact path="/dataimport/earningsreleases" component={EarningsReleasesJSONDataImport} />
-                <Route exact path="/dataimport/ratingscales" component={ImportRatingScales} />
-                <Route exact path="/blob" component={Blob} />
-            </Switch>
-        </Router>
+        <BrowserRouter>
+            <Navigation />            
+
+            <Routes>
+                <Route path="/" element={<StocksApp />} />
+                <Route path="/stock" element={<IndividualStock />} />
+                <Route path="/upcomingEarningsReleases" element={<UpcomingEarningsReleases />}/>
+                <Route path="/dataImports" element={<DataImportsMain />}/>
+            </Routes>
+        </BrowserRouter>
         </div>
     )
     render(AppRoutes, document.getElementById("render-target"))
