963d1dda428be8fa3e4e7b931391ecaac5019859
Mykhaylo Ignatenko <mykhaylo.ignatenko@gmail.com>
2022-08-21 13:59:28 -0700

    rating changes indices

--- server/collection_indices.js
+++ server/collection_indices.js
@@ -1,7 +1,10 @@
+RatingChanges._ensureIndex({dateString: 1, researchFirmId: 1, symbol: 1});
+RatingChanges._ensureIndex({dateString: 1, symbol: 1});
+RatingChanges._ensureIndex({researchFirmId: 1, symbol: 1, newRatingId: 1, oldRatingId: 1, dateString: 1});
+RatingChanges._ensureIndex({newRatingId: 1});
 RatingChanges._ensureIndex({dateString: 1});
 RatingChanges._ensureIndex({symbol: 1});
-RatingChanges._ensureIndex({dateString: 1, symbol: 1});
-RatingChanges._ensureIndex({newRatingId: 1});
+RatingChanges._ensureIndex({isError: 1});
 
 EarningsReleases._ensureIndex({reportDateNextFiscalQuarter: 1, symbol: 1});
 EarningsReleases._ensureIndex({reportDateNextFiscalQuarter: 1});
