785a4060f462311dc412b4b8d1a48153d176acef
Mykhaylo Ignatenko <mykhaylo.ignatenko@gmail.com>
2022-09-08 00:39:48 -0700

    add checks to publications

--- server/publications.js
+++ server/publications.js
@@ -1,29 +1,38 @@
+import { check } from 'meteor/check';
+
 Meteor.publish("settings", function () {
     return Settings.find({type: "main"}, {fields: {_id: 1, clientSettings: 1}});
 });
 
-
 // RatingChanges publications
 Meteor.publish("ratingChangesForSymbols", function (symbolsArr, start_YYYY_MM_DD, end_YYYY_MM_DD) {
+    check(symbolsArr, [String]);
+    check(start_YYYY_MM_DD, String);
+    check(end_YYYY_MM_DD, String);
+
     return RatingChanges.find({
         symbol: {$in: symbolsArr}, $and: [{dateString: {$gte: start_YYYY_MM_DD}}, {dateString: {$lte: end_YYYY_MM_DD}}]
     }, {
-        fields: {_id: 1, symbol: 1, date: 1, dateString: 1, oldRatingId: 1, newRatingId: 1, researchFirmId: 1}
-    }, {
-        sort: {dateString: 1}
+        fields: {_id: 1, symbol: 1, date: 1, dateString: 1, oldRatingId: 1, newRatingId: 1, researchFirmId: 1},
+        sort: {dateString: 1},
     });
 });
 Meteor.publish("allRatingChangesForSymbol", function (symbol) {
-    return RatingChanges.find(
-        {symbol: symbol},
-        {fields: {_id: 1, symbol: 1, date: 1, dateString: 1, oldRatingId: 1, newRatingId: 1, researchFirmId: 1}},
-        {sort: {dateString: 1}
+    check(symbol, String);
+
+    return RatingChanges.find({
+        symbol: symbol,
+    }, {
+        fields: {_id: 1, symbol: 1, date: 1, dateString: 1, oldRatingId: 1, newRatingId: 1, researchFirmId: 1},
+        sort: {dateString: 1},
     });
 });
 
 
 // RatingScales publications
 Meteor.publish("specificRatingScales", function(ratingScaleIdsArr) {
+    check(ratingScaleIdsArr, [String]);
+
     return RatingScales.find({_id: {$in: ratingScaleIdsArr}}, {fields: {_id: 1, universalScaleValue: 1, researchFirmId: 1}});
 });
 Meteor.publish("ratingScales", function() {
@@ -32,6 +41,8 @@ Meteor.publish("ratingScales", function() {
 });
 
 Meteor.publish("getPortfolioById", function(portfId) {
+    check(portfId, String);
+
     if (this.userId) {
         //portfolios that are either public or the user is owner
         return Portfolios.find(
